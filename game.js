358e3e04a12f38d289ec8594dfb765e5043cb51e
Christel <christelf966@gmail.com>
2022-04-12 19:50:29 -0300

    the final game logic

--- game.js
+++ game.js
@@ -1,12 +1,8 @@
-const playerSelection = prompt("Please enter an option").toLowerCase();
-const computerSelection = computerPlay();
-
 function computerPlay() {
 
     const computerOptions  = ["Rock","Paper","Scissors"];
     let randomOptions = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    console.log(randomOptions);
-    return randomOptions;
+    return randomOptions; //retorno la opción elegida por la computadora
 
 }
 
@@ -14,6 +10,7 @@ function playRound(playerSelection,computerSelection) {
           
     
            let message = "";
+           let result = "";
 
     //If playerSelection is rock
 
@@ -21,45 +18,83 @@ function playRound(playerSelection,computerSelection) {
         case "rock":
           if (computerSelection === "Rock") {
               message = "Is a tied!";
+              result = "tied";
           } if (computerSelection === "Paper") {
               message = "You Loose! - Paper beats rock"; 
+              result = "loose";
           } if (computerSelection === "Scissors") {
               message = "You Win! - Rock beats Scissors ";
+              result = "win";
           }
           break;
         case "paper":
             if (computerSelection === "Paper") {
                 message = "Is a tied!";
+                result = "tied";
             } if (computerSelection === "Scissors") {
                 message = "You Loose! - Scissors beats Paper"; 
+                result = "loose";
             } if (computerSelection === "Rock") {
                 message = "You Win! - Paper beats rock ";
+                result = "win";
             }  
           break;
         case "scissors":
             if (computerSelection === "Scissors") {
                 message = "Is a tied!";
+                result = "tied";
             } if (computerSelection === "Rock") {
                 message = "You Loose! - Rock beats Scissors"; 
+                result = "loose";
             } if (computerSelection === "Paper") {
                 message = "You Win! - Scissors beats Paper ";
+                result = "win";
             }  
             
 
             
     }
 
-      return message;
+        
+      return result; //retorno quién es el ganador desde el pto de vista del usuario
 }
 
 function game() {
-    let result = "";
+
+    //contadores para llevar el puntaje de computadora y jugador, se inicializan en cero y se les va sumando o no algún puntaje
+    let playerSelection = prompt("Please enter an option").toLowerCase();
+    let computerSelection = computerPlay();
+    let computerScore = 0;
+    let playerScore = 0;
+    let winnerMessage = "";
     for (let i = 0; i < 5; i++) {
-            result =  playRound(playerSelection,computerSelection);
         
+        let finalResult = playRound(playerSelection,computerSelection);
+
+        //si el resultado es igual win entonces se debe asignar un punto al jugador
+        if (finalResult ==="win") {
+                playerScore = playerScore + 1;
+
+        }else if (finalResult ==="loose") {
+
+                computerScore = computerScore + 1;
+        }else{
+              
+            computerScore = computerScore + 1;
+            playerScore = playerScore + 1;
+        }
     }
-    console.log(result);
-    return result;
+            if (computerScore > playerScore) {
+
+                    winnerMessage = "You Loose";
+
+            }else if (playerScore > computerScore) {
+
+                    winnerMessage = "You Win";
+            }else{
+                    winnerMessage = "Tied!";
+            }
+    
     
 }
 
